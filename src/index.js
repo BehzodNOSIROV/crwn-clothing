93afed44b3a7569c269f9480c97c73f7a302b4a3
Behzod <behzodnosirovofficial@gmail.com>
2022-03-15 08:20:57 +0500

    redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import store from './redux/store';
+
 import App from './App';
+import { Provider } from 'react-redux';
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
       <App /> 
-  </BrowserRouter>,
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
