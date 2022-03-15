c2ae724cdc531ccb265eb13d0d2554a4b56d3bc5
Behzod <behzodnosirovofficial@gmail.com>
2022-03-15 11:24:00 +0500

    cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import './header.styles.scss';
+import CartIcon from "../cart-icon/cart-icon.component";
 import { auth } from "../../firebase/firebase.utils";
-const Header =({currentUser})=>(
+
+const Header =({currentUser,hidden})=>(
     <div className="header">
         <Link className="logo-container" to='/'>
             <Logo className="logo"/>    
@@ -18,18 +20,23 @@ const Header =({currentUser})=>(
                 CONTACT    
             </Link>
             {
-                currentUser?
+                currentUser?(
                 <div className="option" onClick={()=>auth.signOut()}>SIGN OUT</div>
-                :
+                ):(
                 <Link className="option" to='/signin'>
                     SIGN IN    
                 </Link>
-            }
+            )}
+            <CartIcon/>
         </div>
+        {
+            hidden?null:<CartDropdown/>
+        }
     </div>
 )
-const mapStateToProps=(state)=>({
-    currentUser:state.user.currentUser
+const mapStateToProps=({user:{currentUser}, cart:{hidden}})=>({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
